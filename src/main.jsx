6dd736e6e2bc24b52e4ccf489b328d2ae30ea820
library <elmosalah74@gmail.com>
2025-04-07 13:03:19 +0200

    تعديلات جديدة على المشروع

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
-<HashRouter>
+<BrowserRouter>
   <App />
-</HashRouter>
+</BrowserRouter>
 
   </StrictMode>,
 )
