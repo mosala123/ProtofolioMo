d6c89df9da3ffbac876be6dab1baab4eefeba913
library <elmosalah74@gmail.com>
2025-04-07 13:32:54 +0200

    وصف التعديلات

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes } from "react-router-dom";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
-<BrowserRouter>
-  <App />
-</BrowserRouter>
+    <BrowserRouter basename="/protofolioME">
+      <Routes>
+
+        <App />
+      </Routes>
+    </BrowserRouter>
 
   </StrictMode>,
 )
